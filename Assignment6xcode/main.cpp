ca30972931bc8cf01e1da231ac2765ebf23eb59a
sheerin chaudhary <sheerinc14@hotmail.com>
2015-07-04 04:45:49 -0400

    Initial Commit

---
+++ Assignment6xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment6xcode
+//
+//  Created by Sheerin Chaudhary on 7/4/15.
+//  Copyright (c) 2015 Sheerin Chaudhary. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
